dfbe17d688e249c9aa8b68081788a05a6e52418d
Mathias Lyder Nikolajsen <32513586+Malyni@users.noreply.github.com>
2017-10-29 20:54:06 +0100

    TreeStump klasse tilføjet

---
+++ src/sp2017g1/TreeStump.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sp2017g1;
+
+/**
+ *
+ * @author nikol
+ */
+public class TreeStump implements NPC {
+    public TreeStump(String name) {
+        
+    }
+}
