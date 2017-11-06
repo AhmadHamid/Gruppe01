3571824f39ea713140095e65f584bad55d4d41da
Lasse <lmarq17@student.sdu.dk>
2017-11-06 11:43:25 +0100

    Timer

--- src/sp2017g1/Timer.java
+++ src/sp2017g1/Timer.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sp2017g1;
 
-/**
- *
- * @author Lasse
- */
-public class Timer {
-    static Thread countdown = new Thread();
-    public static void countdownTimer() throws InterruptedException {
-        for (int i = 60;i>=0;i--) {
-            countdown.sleep(1000);
-            System.out.println(i);
-        
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Timer extends Thread {
+
+    private long startTime = System.currentTimeMillis();
+    private long elapsedTime;
+
+    public void run() {
+        try {
+            while (true) {
+                elapsedTime = System.currentTimeMillis() - startTime;
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Timer.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-}
+
+    public long getElapsedTime() {
+        return elapsedTime;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        // TODO code application logic here
+        Timer te = new Timer();
+        te.start();
+        for(int i = 0; i<20;i++){
+            System.out.println("millis gone: " + te.getElapsedTime());
+            Thread.sleep(1000);
+        }
+        te.interrupt();
+    }
+
 }
