bfe56c772916033acb58e75b32084bd36c2cfa80
Ahmad Hamid <Ahham14@student.sdu.dk>
2017-11-13 11:54:34 +0100

    fix

--- F1/src/sp2017g1/Timer1.java
+++ F1/src/sp2017g1/Timer1.java
@@ -6,7 +6,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class Timer extends Thread {
+public class Timer1 extends Thread {
 
     private long startTime = System.currentTimeMillis();
     private long elapsedTime;
@@ -14,7 +14,7 @@ public class Timer extends Thread {
 
     public static void main(String[] args) throws InterruptedException {
         // TODO code application logic here
-        Timer te = new Timer();
+        Timer1 te = new Timer1();
         te.start();
         for(int i = 600; i>=0;i--){
             System.out.println("Seconds passed: " + te.getElapsedTime()/1000);
@@ -35,7 +35,7 @@ public class Timer extends Thread {
                 Thread.sleep(1000);
             }
         } catch (InterruptedException ex) {
-            Logger.getLogger(Timer.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Timer1.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
